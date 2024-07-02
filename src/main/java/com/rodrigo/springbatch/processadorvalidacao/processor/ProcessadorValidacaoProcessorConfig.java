7b98d19af8ce204f71ee3ff3f9a4b804edf91efe
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-07-02 16:26:49 -0300

    config inicial para estudo

---
+++ src/main/java/com/rodrigo/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -0,0 +1,15 @@
+package com.rodrigo.springbatch.processadorvalidacao.processor;
+
+import com.rodrigo.springbatch.processadorvalidacao.dominio.Cliente;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProcessadorValidacaoProcessorConfig {
+    @Bean
+    public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
+        // TODO: Implementar a lógica aqui...
+        return cliente -> cliente;
+    }
+}
